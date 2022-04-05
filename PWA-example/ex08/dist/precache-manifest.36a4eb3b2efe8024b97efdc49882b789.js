209a7bef4ec76354cfbcab2ea2239d29cc8336f1
kth0027 <so_dream@naver.com>
2022-04-05 13:37:35 +0900

    커밋

---
+++ PWA-example/ex08/dist/precache-manifest.36a4eb3b2efe8024b97efdc49882b789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbe520887a710f8e97b",
+    "url": "/css/chunk-vendors.f9baab0f.css"
+  },
+  {
+    "revision": "ad375be1353eb0f5378205f3c932d6bc",
+    "url": "/img/hello-pwa.ad375be1.png"
+  },
+  {
+    "revision": "c739661d09681b8a6c23b9858c7c659e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b70ec0dcc04b73f688e",
+    "url": "/js/app.1a515c1d.js"
+  },
+  {
+    "revision": "5bbe520887a710f8e97b",
+    "url": "/js/chunk-vendors.d0fcf231.js"
+  },
+  {
+    "revision": "354b9cc61dc84fe4a8d48940d724834b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
